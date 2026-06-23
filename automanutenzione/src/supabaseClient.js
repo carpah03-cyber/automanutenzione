8ee094c31d83a7fd6cffff0c2917610c7dc32641
carpah03-cyber <carpah03@gmail.com>
2026-06-23 13:39:34 +0200

    Delete automanutenzione directory

--- automanutenzione/src/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://gnqghqgfzcbosdsnlvnj.supabase.co'
-const supabaseAnonKey = 'sb_publishable_XzCulHhYqHefGD6s46zwow_ciXm6WWm'
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
